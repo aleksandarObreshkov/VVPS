7566a4e4ef8a08e425370053e34016979788de85
Aleksandar Obreshkov <aleksandar.obreshkov1@gmail.com>
2023-03-25 19:23:24 +0200

    Add accountID header for account operations

--- server/src/main/java/com/example/vvps/controller/AccountController.java
+++ server/src/main/java/com/example/vvps/controller/AccountController.java
@@ -5,6 +5,7 @@ import com.example.vvps.domain.AccountCreationParameters;
 import com.example.vvps.domain.AccountCredentials;
 import com.example.vvps.repository.AccountRepository;
 import com.example.vvps.service.AccountService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,7 +31,8 @@ public class AccountController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<Account> createAccount(@RequestBody AccountCreationParameters accountCreationParameters) {
+    public ResponseEntity<Account> createAccount(@RequestBody AccountCreationParameters accountCreationParameters,
+                                                 @RequestHeader("account_id") String id) {
         Account account = Account.builder()
                 .name(accountCreationParameters.getName())
                 .password(accountCreationParameters.getPassword())
@@ -40,18 +42,29 @@ public class AccountController {
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Account>> getAll() {
+    public ResponseEntity<List<Account>> getAll(@RequestHeader("account_id") String id) {
+        if (!accountRepository.getIsAdminById(UUID.fromString(id))) {
+            return ResponseEntity.status(403).build();
+        }
         return ResponseEntity.ok(accountRepository.findAll());
     }
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Account> getById(@PathVariable String id) {
+    public ResponseEntity<Account> getById(@PathVariable String id,
+                                           @RequestHeader("account_id") String loggedAccountId) {
+        if (!accountRepository.getIsAdminById(UUID.fromString(loggedAccountId))) {
+            return ResponseEntity.status(403).build();
+        }
         return ResponseEntity.of(accountRepository.findById(UUID.fromString(id)));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable String id) {
+    public ResponseEntity<Void> deleteById(@PathVariable String id,
+                                           @RequestHeader("account_id") String loggedAccountId) {
+        if (!accountRepository.getIsAdminById(UUID.fromString(loggedAccountId))) {
+            return ResponseEntity.status(403).build();
+        }
         accountService.deleteById(id);
         return ResponseEntity.status(202).build();
     }
--- server/src/main/java/com/example/vvps/repository/AccountRepository.java
+++ server/src/main/java/com/example/vvps/repository/AccountRepository.java
@@ -2,6 +2,8 @@ package com.example.vvps.repository;
 
 import com.example.vvps.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -10,4 +12,7 @@ import java.util.UUID;
 public interface AccountRepository extends JpaRepository<Account, UUID> {
     Account findByName(String name);
     Account findByNameAndPassword(String name, String password);
+
+    @Query("select a.isAdmin from Account a where a.id=:id")
+    boolean getIsAdminById(@Param("id") UUID id);
 }
